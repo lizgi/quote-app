7f970658b5f6f264753934aa5df7bd826edb5279
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-10-11 01:17:06 +0300

    display the object

--- src/app/post.ts
+++ src/app/post.ts
@@ -1,2 +1,5 @@
 export class Post {
+
+    constructor (public title: string, public description: string, public author: string, public poster: string,public date: string) { 
+    }
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Post } from '../post';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent implements OnInit {
+
+  posts: Post[] =[
+    new Post('first project','failure is opportunity','henry-ford','liz','1/2/2021'),
+    new Post('second project','knowledge and action','johann-wolf','beth','2/4/2021'),
+    new Post('third project','self-believe','john-wooden','shii','11/3/2021'),
+    new Post('fourth project','interfearance','serah-hart','ann','1/5/2021')
+  
+  ]
 
   constructor() { }
 
