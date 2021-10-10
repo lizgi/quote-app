2f668593a8382f6563e90594f1d39801cce53d29
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-10-11 01:20:03 +0300

    add first quote

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,7 +8,7 @@ import { Post } from '../post';
 export class PostsComponent implements OnInit {
 
   posts: Post[] =[
-    new Post('first project','failure is opportunity','henry-ford','liz','1/2/2021'),
+    new Post('first project','Failure is the opportunity to begin again more intelligently.','henry-ford','liz','1/2/2021'),
     new Post('second project','knowledge and action','johann-wolf','beth','2/4/2021'),
     new Post('third project','self-believe','john-wooden','shii','11/3/2021'),
     new Post('fourth project','interfearance','serah-hart','ann','1/5/2021')
