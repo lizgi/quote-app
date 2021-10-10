0beaefa3d29816c2c63dbc7c1cc473bbefa394b4
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-10-11 02:50:48 +0300

    detail component

--- src/app/post-detail/post-detail.component.ts
+++ src/app/post-detail/post-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Input} from '@angular/core';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-post-detail',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-detail.component.css']
 })
 export class PostDetailComponent implements OnInit {
-
+   @Input() posts : Post;
+   
   constructor() { }
 
   ngOnInit(): void {
