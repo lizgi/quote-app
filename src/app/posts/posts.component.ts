35af3b1b6c15af54dd03c8853ef91f3f44b5b8ce
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-10-11 01:23:40 +0300

    3rd quote

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -10,7 +10,7 @@ export class PostsComponent implements OnInit {
   posts: Post[] =[
     new Post('first project','Failure is the opportunity to begin again more intelligently.','henry-ford','liz','1/2/2021'),
     new Post('second project','Knowing is not enough,we must apply.Wishing is not enough,we must do.','johann-wolf','beth','2/4/2021'),
-    new Post('third project','self-believe','john-wooden','shii','11/3/2021'),
+    new Post('third project','Believe in yourself and all that you are.Know there is something inside you that is greater than any obstacle.','john-wooden','shii','11/3/2021'),
     new Post('fourth project','interfearance','serah-hart','ann','1/5/2021')
   
   ]
