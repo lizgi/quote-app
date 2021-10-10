81268f665529fbe618ec0eab79a04fa166fc31ae
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-10-11 01:21:47 +0300

    2nd quote

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -9,7 +9,7 @@ export class PostsComponent implements OnInit {
 
   posts: Post[] =[
     new Post('first project','Failure is the opportunity to begin again more intelligently.','henry-ford','liz','1/2/2021'),
-    new Post('second project','knowledge and action','johann-wolf','beth','2/4/2021'),
+    new Post('second project','Knowing is not enough,we must apply.Wishing is not enough,we must do.','johann-wolf','beth','2/4/2021'),
     new Post('third project','self-believe','john-wooden','shii','11/3/2021'),
     new Post('fourth project','interfearance','serah-hart','ann','1/5/2021')
   
