e80b40249a9e59665e85281ff006d19a7f1e0a4e
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-10-11 12:09:52 +0300

    delete and add home component

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,10 +8,10 @@ import { Post } from '../post';
 export class PostsComponent implements OnInit {
 
   posts: Post[] =[
-    new Post('first project','Failure is the opportunity to begin again more intelligently.','henry-ford','liz','1/2/2021'),
-    new Post('second project','Knowing is not enough,we must apply.Wishing is not enough,we must do.','johann-wolf','beth','2/4/2021'),
-    new Post('third project','Believe in yourself and all that you are.Know there is something inside you that is greater than any obstacle.','john-wooden','shii','11/3/2021'),
-    new Post('fourth project','Dont let what you cannot do interfere with what you can do.','serah-hart','ann','1/5/2021')
+    new Post('first quote','Failure is the opportunity to begin again more intelligently.','henry-ford','liz','1/2/2021'),
+    new Post('second quote','Knowing is not enough,we must apply.Wishing is not enough,we must do.','johann-wolf','beth','2/4/2021'),
+    new Post('third quote','Believe in yourself and all that you are.Know there is something inside you that is greater than any obstacle.','john-wooden','shii','11/3/2021'),
+    new Post('fourth quote','Dont let what you cannot do interfere with what you can do.','serah-hart','ann','1/5/2021')
   
   ]
 
