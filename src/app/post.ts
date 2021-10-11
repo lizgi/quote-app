1f209364e611da2c1d051de27c70f4e475dd3e1c
lizgi <elizabeth.gikonyo@student.moringaschool.com>
2021-10-11 18:56:59 +0300

    adds paragraph

--- src/app/post.ts
+++ src/app/post.ts
@@ -1,6 +1,6 @@
 export class Post {
   showDescription: boolean;
-    constructor (public title: string, public description: string, public author: string, public poster: string,public date: string) {
+    constructor (public title: string, public description: string, public author: string, public poster: string,public date: string,public upvote: string,public downvote: string,public delete: string ) {
         this.showDescription = false; 
     }
     
